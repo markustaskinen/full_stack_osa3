ab01dd50299eec4f61c4e0ace14953533f826b57
markustaskinen <markustaskinen@gmail.com>
2021-01-27 21:15:37 +0200

    added frontend

---
+++ build/precache-manifest.5c50b088e7221002ceda2a40de04c0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2403689f0ce1c8b47f3545aa7fdb063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a551d7e19e765a3a91",
+    "url": "/static/css/main.3cb8e4f6.chunk.css"
+  },
+  {
+    "revision": "f96d806b3f55726b8a94",
+    "url": "/static/js/2.40695959.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40695959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a551d7e19e765a3a91",
+    "url": "/static/js/main.3b820037.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
